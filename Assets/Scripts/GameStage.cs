113d3b474bfe3be9a3a72c356c2f0169d9448c0c
HS <blue12342000@gmail.com>
2021-06-23 23:49:10 +0900

    Cinemachine Follow Zoom 추가

---
+++ Assets/Scripts/GameStage.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[Serializable]
+public struct EnemyData
+{
+    [SerializeField]
+    EnemyState initState;
+    [SerializeField]
+    Vector3 spawnPoint;
+    [SerializeField]
+    Vector3 movePoint;
+    [SerializeField]
+    float attackSpeed;
+}
+
+[Serializable]
+public struct EnemyGroup
+{
+    [SerializeField]
+    string enemyId;
+    [SerializeField]
+    List<EnemyData> enemyDatas;
+}
+
+[RequireComponent(typeof(BoxCollider))]
+public class GameStage : MonoBehaviour
+{
+
+    [SerializeField]
+    List<EnemyGroup> enemyGroups;
+    CinemachineDollyCart dollyCart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        dollyCart = other.GetComponent<CinemachineDollyCart>();
+        if (dollyCart)
+        {
+            //
+            dollyCart.enabled = false;
+        }
+    }
+}
