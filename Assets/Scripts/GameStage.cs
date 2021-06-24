3bded5514b57fd1fbf6b51ef808a1baf2f2ecab2
KSL <kslee9512@gmail.com>
2021-06-24 09:49:48 +0900

    06_24_Ninja_Run Animation

--- Assets/Scripts/GameStage.cs
+++ Assets/Scripts/GameStage.cs
@@ -33,7 +33,6 @@ public class GameStage : MonoBehaviour
     [SerializeField]
     List<EnemyGroup> enemyGroups;
     CinemachineDollyCart dollyCart;
-
     // Start is called before the first frame update
     void Start()
     {
