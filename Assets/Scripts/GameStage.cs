429417f765367bec6919f4d49c2328af54f77f62
HS <blue12342000@gmail.com>
2021-06-24 15:00:28 +0900

    돌리카트 시네 카메라 클리어샷 적용

--- Assets/Scripts/GameStage.cs
+++ Assets/Scripts/GameStage.cs
@@ -8,6 +8,8 @@ using Cinemachine;
 public struct EnemyData
 {
     [SerializeField]
+    float spawnTime;
+    [SerializeField]
     string enemyId;
     [SerializeField]
     GameObject enemyObj;
@@ -31,20 +33,48 @@ public struct EnemyGroup
 [RequireComponent(typeof(BoxCollider))]
 public class GameStage : MonoBehaviour
 {
+    [Header("스테이지 클리어정보")]
+    [SerializeField]
+    string name;
+    [SerializeField]
+    int score;
+    [SerializeField]
+    float clearTime;
 
+    float eventTimer;
+    Queue<EnemyGroup> readyEnemyGroups;
+    EnemyGroup currGroup;
+    List<Enemy> enemys;
+
+    [Header("스테이지 적 스폰정보")]
     [SerializeField]
     List<EnemyGroup> enemyGroups;
     CinemachineDollyCart dollyCart;
+    CinemachineBrain cinemachineBrain;
+
+    delegate void killScore(Enemy enemy);
+
+    public bool IsPlay { get; set;}
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cinemachineBrain = Camera.main.GetComponent<CinemachineBrain>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsPlay)
+        {
+            eventTimer += Time.deltaTime;
+
+            if (eventTimer > 3.0f)
+            {
+                dollyCart.enabled = true;
+                IsPlay = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -53,7 +83,16 @@ public class GameStage : MonoBehaviour
         if (dollyCart)
         {
             //
+            cinemachineBrain.ActiveVirtualCamera.LookAt = transform;
+            //cinemachineBrain.ActiveVirtualCamera.Follow = null;
             dollyCart.enabled = false;
+
+            readyEnemyGroups = new Queue<EnemyGroup>(enemyGroups);
+            eventTimer = 0f;
+            score = 0;
+            clearTime = 0;
+
+            IsPlay = true;
         }
     }
 }
