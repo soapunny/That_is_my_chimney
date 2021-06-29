4b207998814a5278b8000ba8a46a2fdb0c2cb382
kslee9512 <kslee9512@gmail.com>
2021-06-29 12:31:18 +0900

    06_29_stage1,2 Rank

--- Assets/Scripts/GameStage.cs
+++ Assets/Scripts/GameStage.cs
@@ -107,7 +107,7 @@ public class GameStage : MonoBehaviour
                         dollyCart.enabled = true;
                         virtualCamera.Priority = 0;
                         //virtualCamera.gameObject.SetActive(false);
-                        player.SetActive(true);
+                        //player.SetActive(true);
 
                         GameManager.gameManager.currStage = null;
                         isStart = false;
@@ -193,7 +193,7 @@ public class GameStage : MonoBehaviour
             //cinemachineBrain.ActiveVirtualCamera.LookAt = transform;
             //cinemachineBrain.ActiveVirtualCamera.Follow = null;
             virtualCamera.Priority = 20;
-            player.SetActive(false);
+            //player.SetActive(false);
             Debug.Log(virtualCamera.name);
             readyEnemyGroups = new Queue<EnemyGroup>(enemyGroups);
             isStart = NextEnemyGroup();
